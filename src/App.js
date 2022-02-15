3b0878399633995d5f5a26f49f022e564d1b6395
Luis Alain <luisalain230904@gmail.com>
2022-02-15 18:20:44 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import Axios from 'axios';
+import Coin from './coin';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    Axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=false').then(response => {
+      setCoins(response.data);
+    }).catch(error => console.log(error));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())  
+  )
+
+  return(
+    <div className='App'>
+      <div className='coin-search'>
+        <h1 className='acronym'>Crypto
+          <span className='acronym-color'>View</span>
+        </h1>
+        <form>
+          <input type={'search'} placeholder='search a currency' className='coin-input' onChange={handleChange}/>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return <Coin key={coin.id}
+          name={coin.name} 
+          image={coin.image} 
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+        />
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
